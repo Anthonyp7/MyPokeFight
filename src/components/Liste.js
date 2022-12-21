6a201ea836c40de67babeafff6c668e84d83eeda
Anthony Perozeni <anthony.perozeni777@gmail.com>
2022-12-21 01:51:00 +0100

    Affichage deuxième type

--- src/components/Liste.js
+++ src/components/Liste.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import '../styles/Liste.css';
 import '../styles/PokemonColors.css';
 
@@ -18,8 +18,11 @@ export default function ListePokemon(){
   const [height, setHeight] = useState("");
   const [weight, setWeight] = useState("");
 
-  const [testbool, setTestBool] = useState(true);
-  // const testbool = true;
+  // const [testbool, setTestBool] = useState(true);
+
+  const [types, setTypes] = useState(true);
+  let testbool = true;
+  const count = useRef(0);
 
 
 
@@ -80,28 +83,29 @@ export default function ListePokemon(){
         setImage2((current) => [...current, data.sprites.front_shiny]);
         setHeight((current) => [...current, data.height]);
         setWeight((current) => [...current, data.weight]);
+        setType((current) => [...current, data.types.map(type => type.type.name + " ")]);
         // setType((current) => [...current, data.types[0].type.name]);
         
-        if((data.types).length === 1)
-        {
-          setType((current) => [...current, data.types[0].type.name]);
-          setTestBool(false);
-          console.log("faux");
-        }
-        else{
-          setType((current) => [...current, data.types[0].type.name]);
-          setType2((current) => [...current, data.types[1].type.name]);
-          setTestBool(true);
-          console.log("vrai");
-        }
-
-        console.log(testbool);
-        // {pokemon.types.map((typepoke, index) => {
-          
-        //   setType((current) => [...current, data.types[index].type.name]);
         
-        // })}
+        // if((data.types).length === 1)
+        // {
+        //   console.log((data.types).length);
+        //   setType((current) => [...current, data.types[0].type.name]);
+        //   setTypes(false);
+        //   testbool = false;
+        // }
+        // else{
+        //   console.log((data.types).length);
+        //   setType((current) => [...current, data.types[0].type.name]);
+        //   setType2((current) => [...current, data.types[1].type.name]);
+        //   setTypes(true);
+        //   testbool = true;
+        // }
+
+        // console.log(types);
+        console.log(testbool);
         console.log(data.types[1].type.name);
+        count.current = testbool;
       })
       
       // .then((data) => {
@@ -112,7 +116,7 @@ export default function ListePokemon(){
 
       .catch((err) => console.error(err))
     ))
-  }, [pokemons]);
+  }, [pokemons], [testbool]);
 
 
   
@@ -130,33 +134,52 @@ export default function ListePokemon(){
         {pokemons.map((pokemon, index) => (
             
           <div class="card text-bg-dark mb-3" id="test">
-              <div class="card-body">
-                  <h5 class="card-title" key={index}>{index+1} • {pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}</h5>
+            <div class="card-body">
+              <h5 class="card-title" key={index}>{index+1} • {pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}</h5>
 
-                  <div class="image">
-                    <img className="img-poke" src={image[index]} alt="{pokemon}" />
-                  </div>
-                  <div class="image-hover">
-                    <img className="img-poke" src={image2[index]} alt="{pokemon}" />
-                  </div>
+              <div class="image">
+                <img className="img-poke" src={image[index]} alt="{pokemon}" />
+              </div>
+              <div class="image-hover">
+                <img className="img-poke" src={image2[index]} alt="{pokemon}" />
+              </div>
+
+{/* {testbool === true ? <div><ul class={type2[index]}>
+                      <li class="list-group-item">{type2[index]}</li>
+                    </ul></div> : <h4>test</h4> } */}
 
 
+                    {/* {console.log("test", count.current)}
+                    {testbool === true ? <div><p>Type 1 et Type 2</p></div>
+                    : <div><p>Type 1</p></div>} */}
 
                   {/* INFO POKEMON */}
+                  
+
+                  {console.log(type[index])}
                   <div  className="type-li" >
                     <ul class={type[index]}>
                       <li class="list-group-item">{type[index]}</li>
                     </ul>
 
-                {/* {testbool === true ? <div><ul class={type2[index]}>
-                      <li class="list-group-item">{type2[index]}</li>
-                    </ul></div> : <h4>test</h4> } */}
-
-                    {/* {console.log(testbool)} */}
-
-                    <ul class={type2[index]}>
-                      <li class="list-group-item">{type2[index]}</li>
+                    <ul class={type[index]}>
+                      <li class="list-group-item">{type[index]}</li>
                     </ul>
+
+
+                    {/* {type[index][1] != null ? 
+                    <ul class={type[index][1]}>
+                      <li class="list-group-item">{type[index][1]}</li>
+                    </ul>: 
+                    <ul class={type[index]}>
+                      <li class="list-group-item">{type[index]}</li>
+                    </ul>} */}
+
+
+
+                    {/* <ul class={type2[index]}>
+                      <li class="list-group-item">{type2[index]}</li>
+                    </ul> */}
                   </div>
 
                     <ul class="">
