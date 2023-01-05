e49e0fcb6b97538cb0388a19f33a6a372b20851c
Anthony Perozeni <anthony.perozeni777@gmail.com>
2023-01-05 23:52:23 +0100

    Essaie modifs Signup (erreur end of input)

--- front/src/components/SignUp.js
+++ front/src/components/SignUp.js
@@ -4,23 +4,42 @@ export default function SignUp() {
     const [username, setUsername] = useState("Username");
     const [password, setPassword] = useState("Password");
 
-    fetch("http://localhost:3080/login", {
-      method: "POST",
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
+    const handleSubmit = async () => {
+      console.log(username, password);
+  
+      fetch("http://localhost:3080/login", {
+        mode: 'no-cors',
+        method: "POST",
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+      // .then((data) => {
+      //   if (data.msg === undefined) {
+      //     alert(data.error);
+      //     setUsername("");
+      //     setPassword("");
+      //   }
+      //   if (data.msg !== undefined) {
+      //     alert(data.msg);
+      //     setUsername("");
+      //     setPassword("");
+      //   }
+      // })
+        .catch((err) => console.log(err));
+    }
 
     return (
         <>
         <h1>Sign Up</h1>
-        <input type="text" class="form-control" id="floatingInput" placeholder={username}/>
-        <input type="password" class="form-control" id="floatingInput" placeholder={password}/>
-            <input type="button" value="Random Number!"/>
+        <input type="text" onChange={(e) => {setUsername(e.target.value)}} placeholder={username}/>
+        <input type="password" onChange={(e) => {setPassword(e.target.value)}} placeholder={password}/>
+            <input type="button" value="Random Number!" onClick={handleSubmit}/>
         </>
     )
 }
