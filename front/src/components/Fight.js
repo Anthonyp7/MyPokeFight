0e276e13ebc1cf9e24681e98c96c0ecac6ca650b
Anthony Perozeni <anthony.perozeni777@gmail.com>
2023-01-13 14:08:42 +0100

    ras debut fight.js

--- front/src/components/Fight.js
+++ front/src/components/Fight.js
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react"
 
+    
 
 export default function Fight() {
+
+    const [pokeName, setPokeName] = useState("");
+
+    const [pokeHeight, setPokeHeight] = useState("");
+    const [pokeWeight, setPokeWeight] = useState("");
+        
+
+    // INFO STATS POKEMON Card
+    const [pokeHp, setPokeHp] = useState("");
+    const [pokeAttack, setPokeAttack] = useState("");
+    const [pokeSpeed, setPokeSpeed] = useState("");
+    
+    useEffect(() => {
+
+    })
     return(
         <>
             <h2>Fight</h2>
