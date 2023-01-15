88f4730ea5acfd5a17e067ca468a314be51ea63c
Anthony Perozeni <anthony.perozeni777@gmail.com>
2023-01-15 19:44:11 +0100

    Avancement boutton ajout fight (order)

--- front/src/components/Card.js
+++ front/src/components/Card.js
@@ -1,92 +1,94 @@
-// import Button from 'react-bootstrap/Button';
-// import Card from 'react-bootstrap/Card';
-
-
-// export default function CardPokemon(x) {
-//     return(
-//         <Card style={{ width: '18rem' }}>
-//             <Card.Img variant="top" src={x}/>
-//             <Card.Body>
-//             <Card.Title>Card Title</Card.Title>
-//             <Card.Text>
-//                 Some quick example text to build on the card title and make up the
-//                 bulk of the card's content.
-//             </Card.Text>
-//             <Button variant="primary">Go somewhere</Button>
-//             </Card.Body>
-//         </Card>
-//     )
-
-// }
-
-
-
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import Fight from './Fight';
 
 
 export default function CardPokemon(props) {
+    const ls = localStorage;
     const [show, setShow] = useState(false);
     const handleShow = () => setShow(true);
+
+    const [test, setTest] = useState([]);
+    const [order, setOrder] = useState([]);
+    const [tab, setTab] = useState([]);
+
+    const AddtoFight = () => {
+        // if (test.length < 1){
+        //     setTab(test.push(props.id));
+        // }
+        // console.log("test", test);
+
+        // // for (let i = 0; i >= 4; i += 1){
+        // //     setTab(order.push(test));
+        // // }
+
+        // setTab(order.push(test));
+        
+        
+        // ls.setItem("Order", JSON.stringify(order));
+        // console.log("order", order);
+        console.log("Addfight");
+        <Fight 
+        id={0}/>
+        
+    }
+
+    
 
     return (
         <>
         {props.fight === true ?
-            
-                
-                    
-                
-                <>
-                    <Card {...props} bg="light" style={{ width: '12rem' , display: 'inline-block', marginRight: '130px', marginLeft: '150px'}}>
-                        <Card.Img variant="top" src={props.img} />
-                        <Card.Body>
-                            <Button variant="primary">Ajouter au combat</Button>
-                        </Card.Body>
-                    </Card>
-                </>
+            <>
+                <Card {...props} bg="light" style={{ width: '12rem' , display: 'inline-block', marginRight: '130px', marginLeft: '150px', marginBottom: '40px'}}>
+                    <Card.Img variant="top" src={props.img} />
+                    <Card.Body>
+                        {/* <Button variant="primary" onClick={props.addfight}>Ajouter au combat</Button> */}
+                    </Card.Body>
+                </Card>
+            </>
                 
         :
-                <>
-                    <Card {...props} bg="light" style={{ width: '17rem' , display: 'inline-block', marginRight: '100px', marginLeft: '100px'}}>
-                        {props.isShiny === 7 ? 
-                        <>
-                            <Card.Img variant="top" src={props.img2} />
-                        </>
-                        :
-                        <>
-                            <Card.Img variant="top" src={props.img} />
-                        </>}
-                
+            <>
+                <Card {...props} bg="light" style={{ width: '17rem' , display: 'inline-block', marginRight: '100px', marginLeft: '100px', marginBottom: '40px'}}>
+                    {props.isShiny === 7 ? 
+                    <>
+                        <Card.Img variant="top" src={props.img2} />
+                    </>
+                    :
+                    <>
+                        <Card.Img variant="top" src={props.img} />
+                    </>}
+            
 
-                        <Card.Title>{props.name}</Card.Title>
-                        <Card.Body>
-                            <div>
-                                <p className="card-text">Taille: {props.pokeheight} m <br></br>
-                                    Poids: {props.pokeweight} kg</p>
-                            </div>
-                            <div className='progress-stats'>
-                                
-                                <img src='https://cdn-icons-png.flaticon.com/512/7037/7037210.png' alt='' /><br></br>
-                                <ProgressBar animated variant='success' now={props.pokehp} label={`${props.pokehp}`} />
-
-                                <br></br>
-                                
-                                <img src='https://cdn-icons-png.flaticon.com/512/2746/2746914.png' alt='' /><br></br>
-                                <ProgressBar animated variant='danger' now={props.pokeattack} label={`${props.pokeattack}`} />
-                                <br></br>
-                                
-                                <img src='https://cdn-icons-png.flaticon.com/512/7154/7154506.png' alt='' /><br></br>
-                                <ProgressBar animated variant='info' now={props.pokespeed} label={`${props.pokespeed}`} />
-                            </div>
-                            <br></br>
-                        
+                    <Card.Title>{props.name}</Card.Title>
+                    <Card.Body>
+                        <div>
+                            <p className="card-text">Taille: {props.pokeheight} m <br></br>
+                                Poids: {props.pokeweight} kg</p>
+                        </div>
+                        <div className='progress-stats'>
                             
+                            <img src='https://cdn-icons-png.flaticon.com/512/7037/7037210.png' alt='' /><br></br>
+                            <ProgressBar animated variant='success' now={props.pokehp} label={`${props.pokehp}`} />
+
+                            <br></br>
+                            
+                            <img src='https://cdn-icons-png.flaticon.com/512/2746/2746914.png' alt='' /><br></br>
+                            <ProgressBar animated variant='danger' now={props.pokeattack} label={`${props.pokeattack}`} />
+                            <br></br>
+                            
+                            <img src='https://cdn-icons-png.flaticon.com/512/7154/7154506.png' alt='' /><br></br>
+                            <ProgressBar animated variant='info' now={props.pokespeed} label={`${props.pokespeed}`} />
+                        </div>
+                        <br></br>
+                    
                         
-                        </Card.Body>
-                    </Card>
-                </>
+                    
+                    </Card.Body>
+                </Card>
+            </>
 
                 
 
--- front/src/components/Fight.js
+++ front/src/components/Fight.js
@@ -1,23 +1,15 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
 import CardPokemon from "./Card";
 
     
 
-export default function Fight() {
+export default function Fight(props) {
 
     const ls = localStorage;
-    const [pokeName, setPokeName] = useState("");
-
-    const [pokeHeight, setPokeHeight] = useState("");
-    const [pokeWeight, setPokeWeight] = useState("");
-        
-
-    // INFO STATS POKEMON Card
-    const [pokeHp, setPokeHp] = useState("");
-    const [pokeAttack, setPokeAttack] = useState("");
-    const [pokeSpeed, setPokeSpeed] = useState("");
-
-
+    const [order, setOrder] = useState([]);
+    const [tab, setTab] = useState([]);
+    
     const test = JSON.stringify(ls.getItem("PokeId"));
 
     // REMPLACEMENT DES CROCHETS
@@ -25,26 +17,47 @@ export default function Fight() {
 
     // REMPLACEMENT DES GUILLEMETS
     const lspokemon = test2.replace(/["]/g, "").split(",") ;
-
     
-    console.log(test)
-    console.log(lspokemon);
     
     // useEffect(() => {
 
     // })
+
+    // const AddtoFight = (poke) => {
+    //     // setTab(order.push(poke));
+    //     console.log("order", order);
+    // }
+    
+    const AddtoFight = () => {
+        setTab(order.push(props.id));
+        ls.setItem("Order", JSON.stringify(order));
+        console.log("order", order);
+    }
+    
+
     return(
         <>
-            <h2>Fight</h2>
+            <h1 className="load-h1">Fight</h1>
+            <br></br><br></br>
 
             {/* {lspokemon[2]} */}
-            {lspokemon.map((pokemon, index) => (
-                console.log(pokemon),
-                <CardPokemon 
-                img={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon}.png`}
-                isShiny="0"
-                fight={true}
-                />
+            {lspokemon.reverse().map((pokemon, index) => (
+                <>
+                
+                    <CardPokemon 
+                    img={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon}.png`}
+                    id={pokemon}
+                    isShiny="0"
+                    fight={true}
+                    test={0}
+                    addfight={AddtoFight}
+                    />
+                    <Button variant="primary" onClick={() => {
+                        setTab(order.push(pokemon));
+                        ls.setItem("Order", JSON.stringify(order));
+                        console.log("order", order);
+                    }}>Ajouter au combat</Button>
+                </>
             ))}
             
         </>
